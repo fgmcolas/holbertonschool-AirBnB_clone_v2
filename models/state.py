06568dd253a79b230f6457ba116055806ee5c159
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-26 14:36:27 +0100

    modified city and state to use sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
+
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if storage_type == 'db':
+        cities = relationship("City", back_populates="state", cascade="all",
+                              passive_deletes=True)
+    else:
+        @property
+        def cities(self):
+            """Return the list of City instances with state.id equal to
+            current State.id for filestorage
+            """
+            state_cities_list = []
+            from models import storage
+            from models.city import City
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if self.id == city.state.id:
+                    state_cities_list.append(city)
+            return state_cities_list
