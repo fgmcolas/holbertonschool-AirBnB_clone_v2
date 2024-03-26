400a4ba8c8b4095b8329aeb8c2e30fb6bfcf9a22
fgmcolas <https://github.com/fgmcolas>
2024-03-26 19:26:25 +0100

    testing review change

--- models/review.py
+++ models/review.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
+import os
+
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class Review(BaseModel, Base):
@@ -11,3 +14,6 @@ class Review(BaseModel, Base):
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
+    if storage_type == "db":
+        place = relationship("Place", backref="reviews", cascade="delete")
