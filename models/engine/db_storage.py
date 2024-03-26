68c3f0512baf18504b8f11bd4494f93ec0c01638
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-26 15:56:50 +0100

    rework dbstorage all and fix save

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,12 @@ from sqlalchemy import create_engine
 import os
 from models.base_model import Base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class DBStorage():
@@ -29,20 +35,26 @@ class DBStorage():
 
     def all(self, cls=None):
         """ Query all objects by class name,or all if cls=None"""
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        class_map = {
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
+        }
 
         list_objects = {}
         if cls:
+            if isinstance(cls, str):
+                cls = class_map.get(cls, None)
+                if cls is None:
+                    print("** class doesn't exist **")
+                    return list_objects
             self.query_session(cls, list_objects)
         else:
-            classes = [User, State, City, Amenity, Place, Review]
-            for cls in classes:
-                self.query_session(cls, list_objects)
+            for cls_obj in class_map.values():
+                self.query_session(cls_obj, list_objects)
         return list_objects
 
     def query_session(self, cls, list_objects):
@@ -58,7 +70,7 @@ class DBStorage():
 
     def save(self):
         """ Commit all changes of the current database session"""
-        self.__session.delete
+        self.__session.commit()
 
     def delete(self, obj=None):
         """ Delete from the current db session obj if not none"""
