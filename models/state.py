71a78f84ad36203544d7ab1ada9a80d88125204c
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-26 15:56:18 +0100

    fix relationship between city and state, they're happy now

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ import os
 storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
@@ -21,11 +21,13 @@ class State(BaseModel):
             """Return the list of City instances with state.id equal to
             current State.id for filestorage
             """
-            state_cities_list = []
             from models import storage
             from models.city import City
+
+            state_cities_list = []
             all_cities = storage.all(City)
+
             for city in all_cities.values():
-                if self.id == city.state.id:
+                if city.state_id == self.id:
                     state_cities_list.append(city)
             return state_cities_list
