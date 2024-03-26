cb298e5c3d150f49993852cdc9d9237375214674
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-26 17:14:37 +0100

    fix city looking for state relationship in filestorage mode

--- models/city.py
+++ models/city.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
+
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
@@ -10,4 +13,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state = relationship("State", back_populates="cities")
+    if storage_type == 'db':
+        state = relationship("State", back_populates="cities")
