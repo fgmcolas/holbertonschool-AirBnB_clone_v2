72a177a47269818b058f570fcfbf9367a9ee6471
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-26 09:45:48 +0100

    added comments to setup_mysql_dev and quick cleanup of pycodestyle on premade files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- Create the hbnb_dev_db if it doesn't exist
+-- create user hbnb_dev with pwd hbnb_dev_pwd if it doesn't exist
+-- grants all privilege on hbnb_dev_db and select priv on performance_schema to user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
