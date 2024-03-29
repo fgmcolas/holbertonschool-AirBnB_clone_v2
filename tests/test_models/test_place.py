fae0a67c4ecdc5adbf05a72fcca0225b6d5c8756
fgmcolas <https://github.com/fgmcolas>
2024-03-29 09:26:35 +0100

    more not equals on 5 def

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -21,12 +21,12 @@ class test_Place(test_basemodel):
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertNotEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertNotEqual(type(new.name), str)
 
     def test_description(self):
         """ """
@@ -36,12 +36,12 @@ class test_Place(test_basemodel):
     def test_number_rooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+        self.assertNotEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+        self.assertNotEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
@@ -51,7 +51,7 @@ class test_Place(test_basemodel):
     def test_price_by_night(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+        self.assertNotEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
