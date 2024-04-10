1917fb44cbbb444c1590472778e8c2a116aa6963
fgmcolas <https://github.com/fgmcolas>
2024-04-10 11:43:12 +0200

    adding n9 .html and .py with pycodestyle verified

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def display_states():
+    """Display a HTML page with a list of all State objects"""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def display_state(id):
+    """Display a HTML page with details of a State object"""
+    states = storage.all(State).values()
+    state = next((state for state in states if state.id == id), None)
+    if state:
+        return render_template('9-states.html', state=state)
+    else:
+        return render_template('9-states.html', not_found=True)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
