1efbf5e1864342f8c136d88b4951ef9d8688129f
fgmcolas <https://github.com/fgmcolas>
2024-04-10 19:36:50 +0200

    moving return indentation

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -27,7 +27,7 @@ def display_state_id(id):
     for state in all_states:
         if state.id == id:
             return render_template('9-states.html', selected_state=state)
-        return render_template('9-states.html')
+    return render_template('9-states.html')
 
 
 if __name__ == "__main__":
