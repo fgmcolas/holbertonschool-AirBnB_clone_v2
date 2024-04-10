589d09b6fd09958db47ab4afe0bd334de30b4fa1
fgmcolas <https://github.com/fgmcolas>
2024-04-10 10:16:52 +0200

    editing close cass method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -87,4 +87,5 @@ class DBStorage():
 
     def close(self):
         """call remove() method on the private session attribute"""
-        self.__session.remove()
+        self.reload()
+        self.__session.close()
