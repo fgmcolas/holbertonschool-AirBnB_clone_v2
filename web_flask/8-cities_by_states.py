d4456e4c9a196e9e8cc806f87fb743ad21f62c5a
fgmcolas <https://github.com/fgmcolas>
2024-04-10 20:08:19 +0200

    changing indentation in .html, adding an import in .py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ def teardown_session(exception):
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Display a HTML page with the list of states and their cities"""
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
